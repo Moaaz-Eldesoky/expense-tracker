a53ebf719395097449599aaed0997f24d6891919
“m84288955” <moaaz.salah.eldesoky1@huawei-partners.com>
2025-01-06 16:40:35 +0200

    create the main components and finish welcome page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { WelcomeComponent } from './auth/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+];
